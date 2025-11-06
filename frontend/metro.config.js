4f73d58d4712464bf890cd644c196857e2ac00da
Pawel Konik <konius85@gmail.com>
2025-11-06 21:57:16 +0100

    metro

--- frontend/metro.config.js
+++ frontend/metro.config.js
@@ -1,4 +1,5 @@
 const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+const path = require('path');
 
 /**
  * Metro configuration
@@ -6,6 +7,18 @@ const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
  *
  * @type {import('metro-config').MetroConfig}
  */
-const config = {};
+const config = {
+  watchFolders: [path.resolve(__dirname, 'src')],
+  resolver: {
+    blockList: [
+      /node_modules\/.*\/node_modules\/react-native\/.*/,
+    ],
+  },
+  watcher: {
+    watchman: {
+      deferStates: ['hg.update'],
+    },
+  },
+};
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
